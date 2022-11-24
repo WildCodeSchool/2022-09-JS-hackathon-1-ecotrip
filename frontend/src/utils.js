2c98fc336f10f83ecf511d0ceea4e129cbf52a80
Mounir <85800425+Mounir-mk@users.noreply.github.com>
2022-11-24 17:26:52 +0100

    refactored code

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -10,16 +10,18 @@ const distance = (arrivee, depart) => {
   dist = (dist * 180) / Math.PI;
   dist = dist * 60 * 1.1515;
   dist *= 1.609344;
-  // console.warn(dist, "km");
-  // const elecCar = dist * 0.1;
-  // console.warn(Math.floor(elecCar), "kg CO2e pour une voiture électrique");
-  // const carboncar = dist * 0.21;
-  // console.warn(Math.floor(carboncar), "kg CO2e pour une voiture essence");
-  // const train = dist * 0.0023;
-  // console.warn(train, "kg CO2e pour un trajet en TGV");
-  // const avion = dist * 0.23;
-  // console.warn(avion, "kg CO2e pour un trajet en avion");
+
   return dist;
 };
 
-export default distance;
+const fetchData = async (url, setFilter) => {
+  const data = await fetch(url);
+  if (data.status === 200) {
+    const coordsDepart = await data.json();
+    setFilter(coordsDepart);
+  } else {
+    const errorBody = await data.json();
+    console.error(errorBody, "with API key");
+  }
+};
+export { distance, fetchData };
