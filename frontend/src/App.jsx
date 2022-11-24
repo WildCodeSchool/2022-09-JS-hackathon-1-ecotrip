5d288c0ed8d3962afcdcd026f2a8dd278e5a9a3f
ROBIN KOLASINSKI <robin.kolasinski@gmail.com>
2022-11-24 11:23:11 +0100

    cleaning code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
-  const url = `https://api.opentripmap.com/0.1/en/places/geoname?apikey=${
-    import.meta.env.VITE_API_KEY
-  }`;
   return (
     <div className="App">
 
