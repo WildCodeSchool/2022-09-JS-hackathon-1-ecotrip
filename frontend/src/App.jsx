018e0ffec27b95651d7d761468751da9c4e7a75a
ROBIN KOLASINSKI <robin.kolasinski@gmail.com>
2022-11-24 11:18:03 +0100

    removed counter code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
+  const url = `https://api.opentripmap.com/0.1/en/places/geoname?apikey=${
+    import.meta.env.VITE_API_KEY
+  }`;
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+
     </div>
   );
 }
