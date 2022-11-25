10b13f44ae25939f931cc12899f8cfcdcec3a17d
ROBIN KOLASINSKI <robin.kolasinski@gmail.com>
2022-11-25 10:21:09 +0100

    reverted to functional code, added results for different transportation carbon production

--- frontend/src/components/ResultPage.jsx
+++ frontend/src/components/ResultPage.jsx
@@ -3,19 +3,23 @@ import PropTypes from "prop-types";
 
 function ResultPage({ distanceInKm, startingPoint, destination }) {
   // console.warn(dist, "km");
-  // const elecCar = dist * 0.1;
+  const elecCar = distanceInKm * 0.1;
   // console.warn(Math.floor(elecCar), "kg CO2e pour une voiture électrique");
-  // const carboncar = dist * 0.21;
+  const carboncar = distanceInKm * 0.21;
   // console.warn(Math.floor(carboncar), "kg CO2e pour une voiture essence");
-  // const train = dist * 0.0023;
+  const train = distanceInKm * 0.0023;
   // console.warn(train, "kg CO2e pour un trajet en TGV");
-  // const avion = dist * 0.23;
+  const avion = distanceInKm * 0.23;
   // console.warn(avion, "kg CO2e pour un trajet en avion");
   return (
     <main className="h-screen w-screen bg-blue-300 flex flex-col items-center justify-evenly">
       <h1 className="text-4xl text-white font-bold">
-        {Math.floor(distanceInKm)} km between {startingPoint} and {destination}
+        {distanceInKm.toFixed(2)} km between {startingPoint} and {destination}
       </h1>
+      <p>{elecCar.toFixed(2)} kg CO2e pour une voiture électrique</p>
+      <p>{carboncar.toFixed(2)} kg CO2e pour une voiture essence</p>
+      <p>{train.toFixed(2)} kg CO2e pour un trajet en TGV</p>
+      <p>{avion.toFixed(2)} kg CO2e pour un trajet en avion</p>
     </main>
   );
 }
